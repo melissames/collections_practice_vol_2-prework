fa9f3f312e1f2a0a720e80d98c73bd73a89fa023
Melissa Sames <melissa.sames@gmail.com>
2018-01-16 20:54:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,18 @@ def begins_with_r (array)
       totally_r = false
     end
   }
-
   totally_r
+end
+
+def contain_a (array)
+  contains_a = []
+
+  array.each { |el|
+    el.each { |letter|
+      if letter == 'a'
+        contains_a << el
+      end
+    }
+  }
+  contains_a
 end
